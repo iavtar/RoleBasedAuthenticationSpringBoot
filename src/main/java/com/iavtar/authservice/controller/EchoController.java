5e4c5bf55e4c067f9b8cac252b39e69e777d00c9
iavtar <indra.avatarsingh@gmail.com>
2020-05-01 04:02:56 +0300

    Enabled method level security

--- src/main/java/com/iavtar/authservice/controller/EchoController.java
+++ src/main/java/com/iavtar/authservice/controller/EchoController.java
@@ -2,7 +2,7 @@ package com.iavtar.authservice.controller;
 
 import java.security.Principal;
 
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class EchoController {
 
 	@GetMapping("/")
-	@Secured("ROLE_ADMIN")
-	private String echo(Principal principal) {
+	@PreAuthorize("hasRole('ADMIN')")
+	public String echo(Principal principal) {
 		return principal.getName();
 	}
 
